47b12a6ef4cc4667bf8d231da26041f8687d66d4
JonasJs <joonasjss@gmail.com>
2017-09-11 01:59:24 -0300

    desktop header fix

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
 					<h1>UX / UI DESIGNER <span>& <br>DESENVOLVEDOR</span><br>FRONT-END</h1>
 					<p>Sou apaixonado por criar novas idéias Focando na crianção de experiências limpas e amigáveis.</p>
 					<a href="" class="btn-primary"><i class="fa fa-eye" aria-hidden="true"></i>PORTFÓLIO</a>
-					<a href="" class="btn-secondary"><i class="fa fa-eye" aria-hidden="true"></i>PORTFÓLIO</a>
+					<a href="" class="btn-secondary"><i class="fa fa-download" aria-hidden="true"></i>CURRICULO</a>
 				</div>
 				<div class="col-xl-6 col-lg-5">
 					<img src="assets/images/cel-header.png" class="img-fluid" alt="" width="547" height="600">

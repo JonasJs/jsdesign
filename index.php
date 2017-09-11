365df362fc55f4fefc9b1bc0dc4852f818fc3d8e
JonasJs <joonasjss@gmail.com>
2017-09-11 02:41:07 -0300

    desktop sobre finalized

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="assets/style/main.css">	
 </head>
 <body>
-
+	<!-- MENU -->
 	<nav class="navbar navbar-light">
 		<div class="container">
 			<a class="navbar-brand" href="#">
@@ -46,6 +46,7 @@
 			</ul>
 		</div>
 	</nav>
+	<!-- HEADER -->
 	<header class="header">
 		<div class="container">
 			<div class="row">
@@ -61,7 +62,18 @@
 			</div>
 		</div>
 	</header>
-
+	<!-- SOBRE -->
+	<section class="sobre" id="sobre">
+		<div class="container">
+			<div class="col-xl-8 col-lg-8 content">
+				<h1 class="title">Sobre</h1>
+				<p>Olá, Prazer me chamo Jonas! Tenho 18 anos, Moro no Rio de Janeiro e trabalho como Front-End e UX/UI Designer. Sou Apaioxonado por Design, Tecnologias Web, Robótica e Audiovisual.<br>
+				<br>
+				Gosto de resolver problemas de forma criativa e Tenho sempre buscado fontes de conhecimento e aprendizado para que eu possa sempre esta atualizado, pois meu objetivo é oferecer um otimo serviço e ajuda meus clientes a terem um site receptivos e que funcionem hoje e no futuro com uma otima experiencia de usuario e um otimo design.</p>
+				<a href="" class="btn-primary"><i class="fa fa-envelope" aria-hidden="true"></i>contato</a>
+			</div>
+		</div>
+	</section>
 
 
 </body>

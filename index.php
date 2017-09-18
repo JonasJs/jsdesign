ec2506f0c5557a1952c497cae153da2b9c3e6396
JonasJs <joonasjss@gmail.com>
2017-09-18 02:28:36 -0300

    desktop trabalhos finalized

--- index.php
+++ index.php
@@ -106,6 +106,131 @@
 			</div>
 		</div>
 	</section>
+	<!-- Trabalhos -->
+	<section id="trabalhos">
+		<div class="container">
+			<h1 class="title">Trabalhos</h1>
+			<div class="row content-primary">
+				<div class="col-xl-4 col-lg-4">
+					<figure>
+						<img src="assets/images/trabalho1.png" alt="4esports - Agência de publicidade e marketing digital especializada em sistemas eletrônicos." title="4esports - Agência de publicidade e marketing digital especializada em sistemas eletrônicos.">
+						<figcaption>
+							<div class="content-overlay">
+								<h3>4esports</h3>
+								<p>Agência de publicidade e marketing digital especializada em sistemas eletrônicos.</p>
+								<!-- Link web site -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-link" aria-hidden="true"></i></a>
+								<!-- GitHub -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-github" aria-hidden="true"></i></a>
+								<!-- Behance -->
+								<a href="https://www.behance.net/gallery/54260049/4esports-Corporate-Website" target="_BLANk"><i class="fa fa-behance" aria-hidden="true" ></i></a>
+							</div>
+							<div class="overlay"></div>
+						</figcaption>
+					</figure>
+				</div>
+				<div class="col-xl-4 col-lg-4">
+					<figure>
+						<img src="assets/images/trabalho2.png" alt="EMPREGA.VC - Evento que reúne ícones de diferentes áreas, que doam o seu saber para desenvolvimento das comunidades." title="EMPREGA.VC - Evento que reúne ícones de diferentes áreas, que doam o seu saber para desenvolvimento das comunidades.">
+						<figcaption>
+							<div class="content-overlay">
+								<h3>EMPREGA.VC</h3>
+								<p>Evento que reúne ícones de diferentes áreas, que doam o seu saber para desenvolvimento das comunidades.</p>
+								<!-- Link web site -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-link" aria-hidden="true"></i></a>
+								<!-- GitHub -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-github" aria-hidden="true"></i></a>
+								<!-- Behance -->
+								<a href="https://www.behance.net/gallery/34414347/EMPREGAVC" target="_BLANk"><i class="fa fa-behance" aria-hidden="true" ></i></a>
+							</div>
+							<div class="overlay"></div>
+						</figcaption>
+					</figure>
+				</div>
+				<div class="col-xl-4 col-lg-4">
+					<figure>
+						<img src="assets/images/trabalho3.png" alt="">
+						<figcaption>
+							<div class="content-overlay">
+								<h3>Pet Shop</h3>
+								<p>Pet Shop Online onde você encontra mais de 10.000 Produtos para seu Pet.</p>
+								<!-- Link web site -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-link" aria-hidden="true"></i></a>
+								<!-- GitHub -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-github" aria-hidden="true"></i></a>
+								<!-- Behance -->
+								<a href="https://www.behance.net/gallery/36513863/Pet-Shop" target="_BLANk"><i class="fa fa-behance" aria-hidden="true" ></i></a>
+							</div>
+							<div class="overlay"></div>
+						</figcaption>
+					</figure>
+				</div>
+			</div>
+			<div class="row content-secondary">
+				<div class="col-xl-4 col-lg-4">
+					<figure>
+						<img src="assets/images/trabalho4.png" alt="Ice Cream Ball - A Sorveteria que oferecer mais de 50 sabores diferentes preparados artesanalmente." title="Ice Cream Ball - A Sorveteria que oferecer mais de 50 sabores diferentes preparados artesanalmente.">
+						<figcaption>
+							<div class="content-overlay">
+								<h3>Ice Cream Ball</h3>
+								<p>A Sorveteria que oferecer mais de 50 sabores diferentes preparados artesanalmente.</p>
+								<!-- Link web site -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-link" aria-hidden="true"></i></a>
+								<!-- GitHub -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-github" aria-hidden="true"></i></a>
+								<!-- Behance -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-behance" aria-hidden="true"></i></a>
+							</div>
+							<div class="overlay"></div>
+						</figcaption>
+					</figure>
+				</div>
+				<div class="col-xl-4 col-lg-4">
+					<figure>
+						<img src="assets/images/trabalho5.png" alt="">
+						<figcaption>
+							<div class="content-overlay">
+								<h3>Ice Cream Ball</h3>
+								<p>A Sorveteria que oferecer aos seus clientes mais de 50 sabores diferentes preparados artesanalmente.</p>
+								<!-- Link web site -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-link" aria-hidden="true"></i></a>
+								<!-- GitHub -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-github" aria-hidden="true"></i></a>
+								<!-- Behance -->
+								<a href="https://www.behance.net/gallery/35132669/COFFEE-GRAIN-FINE" target="_BLANk"><i class="fa fa-behance" aria-hidden="true"></i></a>
+							</div>
+							<div class="overlay"></div>
+						</figcaption>
+					</figure>
+				</div>
+				<div class="col-xl-4 col-lg-4">
+					<figure>
+						<img src="assets/images/trabalho6.png" alt="COFFEE GRAIN FINE - A maior loja de café e cafeteiras. Aqui você encontra cafés de ótimo qualidade e sabor" title="COFFEE GRAIN FINE - A maior loja de café e cafeteiras. Aqui você encontra cafés de ótimo qualidade e sabor">
+						<figcaption>
+							<div class="content-overlay">
+								<h3>COFFEE GRAIN FINE</h3>
+								<p>A maior loja de café e cafeteiras. Aqui você encontra cafés de ótimo qualidade e sabor</p>
+								<!-- Link web site -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-link" aria-hidden="true"></i></a>
+								<!-- GitHub -->
+								<a href="#" class="disabled" target="_BLANk"><i class="fa fa-github" aria-hidden="true"></i></a>
+								<!-- Behance -->
+								<a href="https://www.behance.net/gallery/35132669/COFFEE-GRAIN-FINE" target="_BLANk"><i class="fa fa-behance" aria-hidden="true"></i></a>
+							</div>
+							<div class="overlay"></div>
+						</figcaption>
+					</figure>
+				</div>
+			</div>
+			<div id="trabalho-redes">
+				<!-- GitHub -->
+				<a href="https://github.com/JonasJs/" target="_BLANk"><i class="fa fa-github" aria-hidden="true"></i></a>
+				<!-- Behance -->
+				<a href="https://www.behance.net/JonasJss" target="_BLANk"><i class="fa fa-behance" aria-hidden="true"></i></a>
+			</div>
+		</div>
+	</section>
+
 
 </body>
 </html>

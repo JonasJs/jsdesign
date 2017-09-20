4f14b73644d274426c598b520054a14765931e9e
JonasJs <joonasjss@gmail.com>
2017-09-20 10:09:58 -0300

    Html skills initialized

--- index.php
+++ index.php
@@ -14,6 +14,8 @@
     <meta name="description" content="Desenvolvimento front-end e Criação de intefaces com comportamentos e Aplicação dos principais conceitos, princípios, testes de usabilidade e experiência do usuário.">
     <meta property="og:description" content="Desenvolvimento front-end e Criação de intefaces com comportamentos e Aplicação dos principais conceitos, princípios, testes de usabilidade e experiência do usuário.">
     <meta name="twitter:description" content="Desenvolvimento front-end e Criação de intefaces com comportamentos e Aplicação dos principais conceitos, princípios, testes de usabilidade e experiência do usuário.">
+    <!-- DEVICON -->
+	<link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
 
     <link rel="stylesheet" href="assets/style/main.css">	
 </head>
@@ -227,6 +229,98 @@
 				<a href="https://github.com/JonasJs/" target="_BLANk"><i class="fa fa-github" aria-hidden="true"></i></a>
 				<!-- Behance -->
 				<a href="https://www.behance.net/JonasJss" target="_BLANk"><i class="fa fa-behance" aria-hidden="true"></i></a>
+			</div>
+		</div>
+	</section>
+	<section id="habilidades">
+		<div class="container">
+			<h1 class="title">HABILIDADES E RECURSOS</h1>	
+			<div class="icones-habilidades">
+				<div class="item-icon">
+					<i class="devicon-photoshop-plain colored"></i>
+					<p>Photoshop</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-illustrator-plain colored"></i>
+					<p>illustrator</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-javascript-plain colored"></i>
+					<p>JavaScript</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-css3-plain colored"></i>
+					<p>CSS 3</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-html5-plain colored"></i>
+					<p>HTML 5</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-php-plain colored"></i>
+					<p>PHP</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-mysql-plain-wordmark colored"></i>
+					<p>Mysql</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-jquery-plain colored"></i>
+					<p>Jquery</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-nodejs-plain colored"></i>
+					<p>Node.js</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-wordpress-plain colored"></i>
+					<p>Wordpress</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-bootstrap-plain colored"></i>
+					<p>Bootstrap</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-sass-original colored"></i>
+					<p>SASS</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-bower-plain colored"></i>
+					<p>Bower</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-gulp-plain colored"></i>
+					<p>Gulp</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-sourcetree-plain colored"></i>
+					<p>SourceTree</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-git-plain colored"></i>
+					<p>Git</p>
+				</div>
+
+				<div class="item-icon">
+					<i class="devicon-github-plain colored"></i>
+					<p>GitHub</p>
+				</div>
+
 			</div>
 		</div>
 	</section>

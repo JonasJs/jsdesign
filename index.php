3de8d1d6e4a8ade27ea54d3caef4e39d0fdd69a2
JonasJs <joonasjss@gmail.com>
2017-10-08 19:30:02 -0300

    Finalized Depoimentos

--- index.php
+++ index.php
@@ -17,6 +17,12 @@
     <!-- DEVICON -->
 	<link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
 
+	<link rel="stylesheet" type="text/css" href="bower_components/slick-carousel/slick/slick.css"/>
+	<!-- Add the new slick-theme.css if you want the default styling -->
+	<link rel="stylesheet" type="text/css" href="bower_components/slick-carousel/slick/slick-theme.css"/>
+				
+
+
     <link rel="stylesheet" href="assets/style/main.css">	
 </head>
 <body>
@@ -324,6 +330,126 @@
 			</div>
 		</div>
 	</section>
+
+	<section id="depoimentos">
+		<div class="container">
+			<h1 class="title">Depoimentos</h1>
+			<div class="slider">
+				<div class="slider-item col col-md-6">
+					<div class="item">
+						<i class="fa fa-quote-right aspas" aria-hidden="true"></i>
+						<div class="slider-img">
+							<img src="https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-0/p206x206/16711840_964476810353917_4459858857151515069_n.jpg?oh=f2ea12c259d5b4dde473555ba622f488&oe=5A3E645A" alt="">
+						</div>
+						<p>Jonas é extramente talentoso, consegue ser sair bem em qualquer área do universo criativo. Ele te surpreende a cada novo projeto que apresenta e sempre apresenta grandes evoluções.</p>
+						<p class="slider-title">Pablo Alves</p>
+						<p class="slider-subTitle">Social Media | Designer</p>
+						<div class="icones">
+							<!-- Link web site -->
+							<a href="http://www.pabloalves.me/" target="_BLANk"><i class="fa fa-link" aria-hidden="true"></i></a>
+							<!-- LinKedin -->
+							<a href="https://www.linkedin.com/in/pabloalvesdesigner/"  target="_BLANk"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+							<!-- Behance -->
+							<a href="https://www.behance.net/pabloalvesdesigner" target="_BLANk"><i class="fa fa-behance" aria-hidden="true"></i></a>
+						</div>
+					</div>
+				</div>
+				<div class="slider-item col col-md-6">
+					<div class="item">
+						<i class="fa fa-quote-right aspas" aria-hidden="true"></i>
+						<div class="slider-img">
+							<img src="https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-0/p206x206/16711840_964476810353917_4459858857151515069_n.jpg?oh=f2ea12c259d5b4dde473555ba622f488&oe=5A3E645A" alt="">
+						</div>
+						<p>Jonas é extramente talentoso, consegue ser sair bem em qualquer área do universo criativo. Ele te surpreende a cada novo projeto que apresenta e sempre apresenta grandes evoluções.</p>
+						<p class="slider-title">Pablo Alves</p>
+						<p class="slider-subTitle">Social Media | Designer</p>
+						<div class="icones">
+							<!-- Link web site -->
+							<a href="http://www.pabloalves.me/" target="_BLANk"><i class="fa fa-link" aria-hidden="true"></i></a>
+							<!-- LinKedin -->
+							<a href="https://www.linkedin.com/in/pabloalvesdesigner/"  target="_BLANk"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+							<!-- Behance -->
+							<a href="https://www.behance.net/pabloalvesdesigner" target="_BLANk"><i class="fa fa-behance" aria-hidden="true"></i></a>
+						</div>
+					</div>
+				</div>
+				<div class="slider-item col col-md-6">
+					<div class="item">
+						<i class="fa fa-quote-right aspas" aria-hidden="true"></i>
+						<div class="slider-img">
+							<img src="https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-0/p206x206/16711840_964476810353917_4459858857151515069_n.jpg?oh=f2ea12c259d5b4dde473555ba622f488&oe=5A3E645A" alt="">
+						</div>
+						<p>Jonas é extramente talentoso, consegue ser sair bem em qualquer área do universo criativo. Ele te surpreende a cada novo projeto que apresenta e sempre apresenta grandes evoluções.</p>
+						<p class="slider-title">Pablo Alves</p>
+						<p class="slider-subTitle">Social Media | Designer</p>
+						<div class="icones">
+							<!-- Link web site -->
+							<a href="http://www.pabloalves.me/" target="_BLANk"><i class="fa fa-link" aria-hidden="true"></i></a>
+							<!-- LinKedin -->
+							<a href="https://www.linkedin.com/in/pabloalvesdesigner/"  target="_BLANk"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+							<!-- Behance -->
+							<a href="https://www.behance.net/pabloalvesdesigner" target="_BLANk"><i class="fa fa-behance" aria-hidden="true"></i></a>
+						</div>
+					</div>
+				</div>
+				<div class="slider-item col col-md-6">
+					<div class="item">
+						<i class="fa fa-quote-right aspas" aria-hidden="true"></i>
+						<div class="slider-img">
+							<img src="https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-0/p206x206/16711840_964476810353917_4459858857151515069_n.jpg?oh=f2ea12c259d5b4dde473555ba622f488&oe=5A3E645A" alt="">
+						</div>
+						<p>Jonas é extramente talentoso, consegue ser sair bem em qualquer área do universo criativo. Ele te surpreende a cada novo projeto que apresenta e sempre apresenta grandes evoluções.</p>
+						<p class="slider-title">Pablo Alves</p>
+						<p class="slider-subTitle">Social Media | Designer</p>
+						<div class="icones">
+							<!-- Link web site -->
+							<a href="http://www.pabloalves.me/" target="_BLANk"><i class="fa fa-link" aria-hidden="true"></i></a>
+							<!-- LinKedin -->
+							<a href="https://www.linkedin.com/in/pabloalvesdesigner/"  target="_BLANk"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+							<!-- Behance -->
+							<a href="https://www.behance.net/pabloalvesdesigner" target="_BLANk"><i class="fa fa-behance" aria-hidden="true"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="slider-buttons">
+				<button id="sliderNext">
+					<i class="fa fa-arrow-left" aria-hidden="true"></i>
+				</button>
+				<button id="sliderPrev">
+					<i class="fa fa-arrow-right" aria-hidden="true"></i>
+				</button>
+			</div>
+		</div>
+	</section>
+
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="bower_components/slick-carousel/slick/slick.min.js"></script>
+
+	<script>
+		$(document).ready(function(){
+			$('.slider').slick({
+				respondTo: 'window',
+				mobileFirst: true,
+				dots: true,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplay: true,
+  				autoplaySpeed: 3000,
+  				nextArrow: $('#sliderNext'),
+  				prevArrow: $('#sliderPrev'),
+  				responsive: [
+  					{
+				        breakpoint: 520, 
+				        settings: {
+				            slidesToShow: 2, 
+				            slidesToScroll: 1
+				        }
+					}
+				]
+			});
+		});
+	</script>
 
 
 </body>

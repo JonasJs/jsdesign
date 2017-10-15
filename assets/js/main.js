c399304e1e966afafa682b17e4a33e52dc0c7608
JonasJs <joonasjss@gmail.com>
2017-10-15 02:27:40 -0200

    contato and footer desktop finalized

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
-alert('helo')
+$(document).ready(function(){
+	$('.slider').slick({
+		respondTo: 'window',
+		mobileFirst: true,
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+			autoplaySpeed: 3000,
+			nextArrow: $('#sliderNext'),
+			prevArrow: $('#sliderPrev'),
+			responsive: [
+				{
+		        breakpoint: 520, 
+		        settings: {
+		            slidesToShow: 2, 
+		            slidesToScroll: 1
+		        }
+			}
+		]
+	});
+});
--- index.php
+++ index.php
@@ -423,33 +423,78 @@
 		</div>
 	</section>
 
+	<section id="contato">
+		<div class="container">
+			<h1 class="title">Contato</h1>
+			<div class="row">
+				<div class="col col-md-6 box-contato-form">
+					<form>
+						<div class="form-group">
+							<label for="formName">Nome:</label>
+							<input type="text" class="form-control" id="formName" placeholder="Digite seu nome:" name="formName">
+						</div>
+						<div class="form-group">
+							<label for="formEmail">Email:</label>
+							<input type="email" class="form-control" id="formEmail" placeholder="Digite seu Email:" name="formEmail">
+						</div>
+						<div class="form-group">
+							<label for="formAssunto">Assunto:</label>
+							<input type="email" class="form-control" id="formAssunto" placeholder="Digite o assunto:" name="formAssunto">
+						</div>
+						<div class="form-group">
+							<label for="formMenssagem">Menssagem:</label>
+							<textarea class="form-control" id="formMenssagem" rows="10" name="formMenssagem" placeholder="Digite sua menssagem:" name="formMenssagem"></textarea>
+						</div>
+
+						<a href="" class="btn-primary"><i class="fa fa-send" aria-hidden="true"></i>Enviar</a>
+					</form>
+				</div>
+				<div class="col col-md-6 box-contato">
+					<div class="info-contato">
+						<div class="icons">
+							<a href="" id="icon-facebook">
+								<i class="fa fa-facebook-official" aria-hidden="true"></i>
+							</a>
+							<a href="" id="icon-behance">
+								<i class="fa fa-behance" aria-hidden="true"></i>
+							</a>
+							<a href="" id="icon-github">
+								<i class="fa fa-github" aria-hidden="true"></i>
+							</a>
+							<a href="" id="icon-linkedin">
+								<i class="fa fa-linkedin" aria-hidden="true"></i>
+							</a>
+						</div>
+						<div class="info">
+							<div class="infos" id="icon-whatsapp">
+								<i class="fa fa-whatsapp" aria-hidden="true"></i>
+								<p>(21) 97660-3854</p>
+							</div>
+							<div class="infos" id="icon-telefone">
+								<i class="fa fa-phone" aria-hidden="true"></i>
+								<p>(21) 97660-3854</p>
+							</div>
+							<div class="infos" id="icon-email">
+								<i class="fa fa-envelope-o" aria-hidden="true"></i>
+								<p>Joonasjss@gmail.com</p>
+							</div>
+						</div>
+						<i class="fa fa-smile-o" aria-hidden="true"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div class="container">
+			<p>© Copyright 2017. Todos os direitos reservados.</p>
+		</div>
+	</footer>
+
 	<script src="bower_components/jquery/dist/jquery.min.js"></script>
 	<script src="bower_components/slick-carousel/slick/slick.min.js"></script>
-
-	<script>
-		$(document).ready(function(){
-			$('.slider').slick({
-				respondTo: 'window',
-				mobileFirst: true,
-				dots: true,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				autoplay: true,
-  				autoplaySpeed: 3000,
-  				nextArrow: $('#sliderNext'),
-  				prevArrow: $('#sliderPrev'),
-  				responsive: [
-  					{
-				        breakpoint: 520, 
-				        settings: {
-				            slidesToShow: 2, 
-				            slidesToScroll: 1
-				        }
-					}
-				]
-			});
-		});
-	</script>
+	
+	<script src="assets/js/main.js"></script>
 
 
 </body>
